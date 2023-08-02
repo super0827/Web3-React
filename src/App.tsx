b4c8dfa804b908583e2fcd00f8e651a201e4475e
MikeYuen <nhatminh.150596@gmail.com>
2023-08-02 16:48:56 +0700

    W3R-02 initiate ConnectMetaMaskBtn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { Web3ReactProvider } from "@web3-react/core";
+
+import MetaMaskBtn from "./components/MetaMaskBtn";
+import { connectors } from "./connectors";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Web3ReactProvider connectors={connectors}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <MetaMaskBtn />
+        </header>
+      </div>
+    </Web3ReactProvider>
   );
 }
 
